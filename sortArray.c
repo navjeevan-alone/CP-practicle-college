b2af6f9524e62372794b85210f269d8b28b2a1e7
Navjeevan Alone <70945019+navjeevan-alone@users.noreply.github.com>
2023-01-19 14:53:50 +0530

    update sortarray

--- sortArray.c
+++ sortArray.c
@@ -6,13 +6,13 @@ void main()
     printf("Enter the array : ");
     for (int i = 0; i <= 5; i++)
     {
-        scanf("%d", &arr[i]);
+        scanf("%d ", &arr[i]);
     }
     for (int i = 0; i < 5; i++)
     {
         for (int j = i + 1; j <= 5; j++)
         {
-            if (arr[i] > arr[j])
+            if (arr[i] > arr[j])// just change > to < for descending array
             {
                 int temp = arr[i];
                 arr[i] = arr[j];
@@ -20,6 +20,7 @@ void main()
             }
         }
     }
+    printf("Sorted array is : ");
     for (int i = 0; i <= 5; i++)
     {
         printf("%d, ", arr[i]);
