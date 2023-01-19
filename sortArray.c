bc0c89a1d10563d2496839149206f98062d3e2ec
Navjeevan Alone <70945019+navjeevan-alone@users.noreply.github.com>
2023-01-19 14:47:53 +0530

    sort array

---
+++ sortArray.c
@@ -0,0 +1,27 @@
+// enter 5 elements in array and then display the sorted array in ascending order
+#include <stdio.h>
+void main()
+{
+    int arr[5];
+    printf("Enter the array : ");
+    for (int i = 0; i <= 5; i++)
+    {
+        scanf("%d", &arr[i]);
+    }
+    for (int i = 0; i < 5; i++)
+    {
+        for (int j = i + 1; j <= 5; j++)
+        {
+            if (arr[i] > arr[j])
+            {
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+    }
+    for (int i = 0; i <= 5; i++)
+    {
+        printf("%d, ", arr[i]);
+    }
+}
