f5e26e68eaaeb1aa0e6d98b1d63b6940f4802e54
Navjeevan Alone <70945019+navjeevan-alone@users.noreply.github.com>
2023-02-11 12:17:13 +0530

    question 5 complete

--- assign8-5.c
+++ assign8-5.c
@@ -1,1 +1,24 @@
 // wap, to calculate the factorial of number using function with argument and return value
+#include <stdio.h>
+void main()
+{
+    int num, i;
+    int fact(int *);
+    printf("Enter a number: ");
+    scanf("%d", &num);
+    i = fact(&num);
+    printf("The factorial is %d", i);
+}
+int fact(int *x)
+{
+    int f = 1;
+    while (*x > 0)
+    {
+        f = f * (*x);
+        *x = *x - 1;
+    }
+    return f;
+}
+// output 
+// Enter a number: 4
+// The factorial is 24
